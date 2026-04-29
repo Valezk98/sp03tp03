eda8c1c81b8e6ba4926c895a3a56b10dfb20b601
Ale <veraale144@gmail.com>
2026-04-29 14:16:38 -0300

    primera subida

---
+++ app.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { mongoDbCon } from './src/config/dbConfig.mjs';
+import router from './src/routes/superHeroRoutes.mjs';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//configuracion de EJS como motor de vistas
+
+app.set('view engine', 'ejs');
+
+//
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+//Middleware para parsear JSON
+app.use(express.json());
+
+//middleware para parsear datos de formularios
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Conexión
+mongoDbCon();
+
+//Router
+app.use('/api', router)
+
+//Para las rutas no encontradas
+app.use((req,res)=>{
+    res.status(404).send(
+        {
+            mensaje: 'Ruta no encontrada'
+        }
+    )
+})
+
+
+//Iniciar el servidor
+
+app.listen(PORT, ()=>{
+    console.log(`Escuchando servidor en http://localhost:${PORT}`);
+});
